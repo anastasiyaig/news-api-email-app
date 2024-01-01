bfde5d2a61401acc9b1ade23dae88aded6cbf321
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-01-01 16:56:31 +0300

    Send email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import send_email
 
 api_key = 'ee89c4486b5446dcb8f32294f5721f45'
 
@@ -12,8 +13,11 @@ request = requests.get(url)
 # Get a dictionary of data
 content = request.json()
 
+body = ''
 # Access the article titles and descriptions
 for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+    if article['title'] is not None:
+        body = body + article['title'] + "\n" + article['description'] + 2*"\n"
 
+composed_email = str(body).encode("utf-8")
+send_email.send_email(composed_email)
